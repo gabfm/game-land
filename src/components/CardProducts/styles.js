2157c7ea31f0fca004b63572559b41b96d97bf6b
gabfm <gabriel1233217@gmail.com>
2023-06-22 21:31:45 -0300

    Commit V.4

--- src/components/CardProducts/styles.js
+++ src/components/CardProducts/styles.js
@@ -4,11 +4,12 @@ export const StyledCardProducts = styled.li`
   flex-basis: 20%;
   margin: .625rem auto; 
   padding: .625rem;
-  border: .0625rem solid #ccc;
-  border-radius: .3125rem;
+  border: .025rem solid #ccc;
+  border-radius: 0.8rem;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  background: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(61,46,116,1) 100%);
 
   img {
     width: 22.5rem;
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -8,12 +8,13 @@ export const StyledHeader = styled.header`
   flex-direction: row;
   align-items: center;
   justify-content: space-around;
-  background-color: #F0FFFF;
+  background: linear-gradient(0deg, rgba(21,59,60,1) 30%, rgba(0,0,0,1) 100%);
   padding: 0 .625rem; 
 
   h1 {
     margin-bottom: .625rem;
     font-size:2.25rem;
+    color: #ffff;
     
   }
 
@@ -24,7 +25,15 @@ export const StyledHeader = styled.header`
 
     input {
       height: 4rem;
-      background-color: #FFFFFF;
+      background-color: transparent;
+      border-radius: 0.8rem;
+      width: 100%;
+      box-sizing: border-box; 
+      padding: 0 .625rem; 
+    }
+    input:hover {
+      height: 4rem;
+      background-color: #ffff;
       border-radius: 0.8rem;
       width: 100%;
       box-sizing: border-box; 
